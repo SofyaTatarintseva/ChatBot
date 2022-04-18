65e84bdaf8ac6de2bf252b154f7ef71afc743724
Sofya Tatarintseva <satatarin@gmail.com>
2022-04-18 18:16:50 +0300

    Add header to request

--- src/apiChat.js
+++ src/apiChat.js
@@ -1,8 +1,12 @@
 import axios from 'axios'
 
+let header = {
+  headers: {'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'}
+}
+
 export async function initUserChat(config) {
   try {
-    let res = await axios.post('https://biz.nanosemantics.ru/api/2.1/json/Chat.init', config)
+    let res = await axios.post('https://biz.nanosemantics.ru/api/2.1/json/Chat.init', config, header)
     if (res.status === 200) {
       //localStorage.setItem('cuid', res.data.result.cuid.toString())
       return res.data.result.cuid.toString()
@@ -14,7 +18,7 @@ export async function initUserChat(config) {
 }
 export async function initNewUserChat(config) {
   try {
-    let res = await axios.post('https://biz.nanosemantics.ru/api/2.1/json/Chat.init', config)
+    let res = await axios.post('https://biz.nanosemantics.ru/api/2.1/json/Chat.init', config, header)
     if (res.status === 200) {
       localStorage.setItem('cuid', res.data.result.cuid.toString())
       return res.data.result.cuid.toString()
@@ -26,7 +30,7 @@ export async function initNewUserChat(config) {
 }
 export async function readyEvent(config) {
   try {
-    let res = await axios.post('https://biz.nanosemantics.ru/api/bat/nkd/json/Chat.event', config)
+    let res = await axios.post('https://biz.nanosemantics.ru/api/bat/nkd/json/Chat.event', config, header)
     if (res.status === 200) {
       return {
         bot: true,
@@ -40,7 +44,7 @@ export async function readyEvent(config) {
 
 export async function getAnswer(config) {
   try {
-    let res = await axios.post('https://biz.nanosemantics.ru/api/bat/nkd/json/Chat.request', config)
+    let res = await axios.post('https://biz.nanosemantics.ru/api/bat/nkd/json/Chat.request', config, header)
     if (res.status === 200) {
       const cuid = localStorage.getItem('cuid')
       if (cuid != config.cuid) {
